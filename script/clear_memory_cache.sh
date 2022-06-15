841e5950a8413c4ec264f39c07283194e20a48eb
akyo3 <akyo.wyam@gmail.com>
2022-06-15 22:28:22 +0900

    under-consideration

--- script/clear_memory_cache.sh
+++ script/clear_memory_cache.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Note, I'm using "echo 1", because using "echo 3" is not recommended in production.
+# VPSでは使用できそうにないです。 別案を検討します。
 echo 1 | sudo tee /proc/sys/vm/drop_caches > /dev/null
